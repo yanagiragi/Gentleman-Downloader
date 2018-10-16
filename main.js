3f72082cd507b760dd94fcd6fa5cd02d825f1933
yanagiragi <qaz90473@gmail.com>
2018-10-16 16:38:54 +0800

    update main.js

--- main.js
+++ main.js
@@ -1,38 +1,77 @@
-var request = require('request');
-var fs = require('fs');
-var cheerio = require('cheerio');
+const request = require('request');
+const fs = require('fs');
+const cheerio = require('cheerio');
 
-var urls = ['http://g.e-hentai.org/g/472987/704f8a429a/','http://g.e-hentai.org/g/472987/704f8a429a/?p=1','http://g.e-hentai.org/g/472987/704f8a429a/?p=2','http://g.e-hentai.org/g/472987/704f8a429a/?p=3'];
+const StoragePath = './Storage/'
 
-for(var url in urls){
-	request(urls[url], (err, res , body) =>{
+if(require.main === module)
+{
+	if(!fs.existsSync(StoragePath))
+		fs.mkdirSync(StoragePath)
+	
+	let urls = process.argv.splice(2)
+	
+	if(urls.length === 0){
+		console.log("Example: node main.js $url [$url...]")
+	}
+
+	urls.map(url => main(url.replace(/\?p=[0-9]*/, '')))
+}
+
+function getpic(url, path){
+	request(url, (err, res , body) => {
 		if(!err){
-			var $ = cheerio.load(body);
-			for(var b in $('.gdtm a')){				
-				var a = $('.gdtm a')[b];				
-				if(typeof a.type != undefined && a.type == 'tag')
-					getpic(a.attribs.href);
+			let $ = cheerio.load(body)
+			let src = $('#i3 img')[0].attribs.src
+			let filename = path + src.substr(src.lastIndexOf('/')+1)
+			
+			// Only Download if not Exists
+			if(!fs.existsSync(filename)){
+				request.get( {url : src, encoding : 'binary'}, (err, res, body) => {
+					if(!err)
+						fs.writeFileSync(filename, body, 'binary')
+					else
+						console.log(`Err with ${src}`)
+				});
 			}
 		}
 	});
 }
 
-function getpic(url){
-	request(url, (err, res , body) =>{
+function main(urlBase, index=0)
+{
+	let url = (index === 0) ? `${urlBase}` : `${urlBase}?p=${index}`
+	request(url, (err, res, body) =>{
 		if(!err){
-			//console.log(body);
-			var $ = cheerio.load(body);
-			var src = $('#i3 img')[0].attribs.src;
-			var filename = src.substr(src.lastIndexOf('/')+1);
-			request.get( {url : src, encoding : 'binary'}, (err, res, body) => {
-				if(!err){
-					fs.writeFile(filename, body, 'binary');
+
+			let $ = cheerio.load(body);
+			let pageCount = $('.ptt td').length
+			
+			if(index >= pageCount)
+			{
+				return
+			}
+			else{
+				// use japanese name, else use english
+				let title = ($('#gd2 #gj')[0].children.length > 0) ? $('#gd2 #gj')[0].children[0].data : $('#gd2 #gn')[0].children[0].data
+				
+				let path = `${StoragePath}${title}/`
+				if(!fs.existsSync(path)) 
+					fs.mkdirSync(path)
+
+				let pics = $('.gdtm a')
+
+				console.log(`Found ${pics.length} Pics On ${title}[${index}]`)
+
+				for(let i = 0; i < pics.length; ++i){
+					let e = pics[i]
+					if(typeof e.type != undefined && e.type == 'tag'){	
+						getpic(e.attribs.href, path)
+					}
 				}
-				else{
-					console.log('err with ' + src);
-				}
-			});
-			//console.log(filename);
+				
+				main(urlBase, index + 1)	
+			}
 		}
 	});
 }
---
+++ readme.md
@@ -0,0 +1,18 @@
+# E-Hentai Downloader
+
+## How to Use:
+
+    node main.js [urls | urls]
+
+    Examples:
+        node main.js "https://e-hentai.org/g/xxxxxxx/xxxxxxxxxx/" "https://e-hentai.org/g/yyyyyyy/yyyyyyyyyy/?p=1"
+
+## Stored Path:
+
+    ./Storage/${YOUR_URL_TITLE}/*.jpg
+
+## Notes:
+    
+* argument https://e-hentai.org/g/yyyyyyy/yyyyyyyyyy/?p=4 Starts download from page 1
+
+* Default title is Japanese
