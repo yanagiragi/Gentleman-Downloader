81ba846b66d77d10acb8ad999d7d905d42c320ac
yanagiragi <qaz90473@gmail.com>
2019-07-29 06:34:58 +0800

    refactor code

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const request = require('request');
-const fs = require('fs');
 const cheerio = require('cheerio');
+const path = require('path');
+const fs = require('fs-extra');
+const sanitize = require('sanitize-filename')
 
 const StoragePath = './Storage/'
 
 if(require.main === module)
 {
-	if(!fs.existsSync(StoragePath))
-		fs.mkdirSync(StoragePath)
+	fs.ensureDirSync(StoragePath)
 	
-	let urls = process.argv.splice(2)
-	
+	const urls = process.argv.splice(2)	
 	if(urls.length === 0){
 		console.log("Example: node main.js $url [$url...]")
 	}
@@ -18,16 +18,16 @@ if(require.main === module)
 	urls.map(url => main(url.replace(/\?p=[0-9]*/, '')))
 }
 
-function getpic(url, path){
-	request(url, (err, res , body) => {
+function getpic(url, filepath){
+	request(url, (err, res, body) => {
 		if(!err){
-			let $ = cheerio.load(body)
-			let src = $('#i3 img')[0].attribs.src
-			let filename = path + src.substr(src.lastIndexOf('/')+1)
+			const $ = cheerio.load(body)
+			const src = $('#i3 img').attr('src')
+			const filename = path.join(filepath, sanitize(src.split('/').pop()))
 			
 			// Only Download if not Exists
 			if(!fs.existsSync(filename)){
-				request.get( {url : src, encoding : 'binary'}, (err, res, body) => {
+				request.get( { uri : src, encoding : 'binary' }, (err, res, body) => {
 					if(!err)
 						fs.writeFileSync(filename, body, 'binary')
 					else
@@ -40,34 +40,36 @@ function getpic(url, path){
 
 function main(urlBase, index=0)
 {
-	let url = (index === 0) ? `${urlBase}` : `${urlBase}?p=${index}`
+	const url = (index === 0) ? `${urlBase}` : `${urlBase}?p=${index}`
 	request(url, (err, res, body) =>{
 		if(!err){
+			const $ = cheerio.load(body)			
+			const pageCount = $('.ptt td a')
+				.toArray()
+				.filter(x => x.type && x.type === 'tag')
+				.map(x => parseInt(x.children[0].data)) // equal to split of $('.ptt td a').text()
+				.filter(Number.isInteger) // get rid of '<' and '>'
+				.sort()
+				.pop()
 
-			let $ = cheerio.load(body);
-			let pageCount = $('.ptt td').length
-			
-			if(index >= pageCount)
-			{
+			if(index >= pageCount) {
 				return
 			}
-			else{
+			else {
 				// use japanese name, else use english
-				let title = ($('#gd2 #gj')[0].children.length > 0) ? $('#gd2 #gj')[0].children[0].data : $('#gd2 #gn')[0].children[0].data
+				const jpTitle = $('#gd2 #gj').text()
+				const engTitle = $('#gd2 #gn').text()
+				const title = jpTitle
 				
-				let path = `${StoragePath}${title}/`
-				if(!fs.existsSync(path)) 
-					fs.mkdirSync(path)
-
-				let pics = $('.gdtm a')
-
-				console.log(`Found ${pics.length} Pics On ${title}[${index}]`)
+				const filepath = path.join(StoragePath, sanitize(title, { replacement: '_' }))
+				fs.ensureDirSync(filepath)
+				
+				const pics = $('.gdtm')
+				console.log(`Found ${pics.length} Pics On ${title}, pagecount = ${index+1}/${pageCount}`)
 
 				for(let i = 0; i < pics.length; ++i){
-					let e = pics[i]
-					if(typeof e.type != undefined && e.type == 'tag'){	
-						getpic(e.attribs.href, path)
-					}
+					let e = $('a', pics[i])
+					getpic(e.attr('href'), filepath)
 				}
 				
 				main(urlBase, index + 1)	
