17c2f0d8b9bbef74b00e740e3de273e9d146b881
yanagiragi <qaz90473@gmail.com>
2019-10-13 09:59:51 +0800

    Adjust encoding of request

--- src/util.js
+++ src/util.js
@@ -15,7 +15,7 @@ function GetRequestOptions (opts) {
 			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0',
 			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 			'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-			'Accept-Encoding': 'gzip, deflate, br',
+			// 'Accept-Encoding': 'gzip, deflate, br',
 			'DNT': '1',
 			'Connection': 'keep-alive',
 			'Upgrade-Insecure-Requests': '1',
