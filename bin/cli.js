5679c7cf266114d280d0efa7b4ec2a05af1b3cea
yanagiragi <qaz90473@gmail.com>
2020-03-12 18:37:29 +0800

    Add exception catch and restore err function

--- bin/cli.js
+++ bin/cli.js
@@ -6,7 +6,9 @@ const sanitize = require('sanitize-filename')
 const { EH, NH, Wnacg, Ahri } = require('..')
 const RequestAsync = require('request-promise')
 
-const StoragePath = __dirname + '/Storage/'
+const StoragePath = path.join(__dirname, 'Storage')
+const errFilesPath = path.join(__dirname, 'Storage', 'err.json')
+const errFiles = JSON.parse(fs.readFileSync(errFilesPath))
 
 if(require.main === module)
 {
@@ -27,7 +29,7 @@ if(require.main === module)
 	})()
 }
 
-async function Process(url)
+async function ProcessUrl(url)
 {
     let agent;
     if (url.includes('exhentai')) {
@@ -46,23 +48,55 @@ async function Process(url)
 		agent = new Ahri(url)
 	}
     
-    // Setup titles, totalPageCount, MetaDatas
-    await agent.Setup()
+    try {
+        // Setup titles, totalPageCount, MetaDatas
+        await agent.Setup()
 
-    // Fetch Image Src
-    await agent.Run()
+        // Fetch Image Src
+        await agent.Run()
 
-    // Start Download Image, note we do not consider naming collision issues when create folder
-    const filepath = path.join(StoragePath, sanitize(agent.title, { replacement: '_' }))
-    fs.ensureDirSync(filepath)
+        // Start Download Image, note we do not consider naming collision issues when create folder
+        const filepath = path.join(StoragePath, sanitize(agent.title, { replacement: '_' }))
+        fs.ensureDirSync(filepath)
 
-    for(let i = 0; i < agent.pics.length; ++i) {
-        const url = agent.pics[i].href
-        const id = agent.pics[i].id
+        for(let i = 0; i < agent.pics.length; ++i) {
+            const url = agent.pics[i].href
+            const id = agent.pics[i].id
+            const options = { uri : url, encoding : 'binary' }
+            const extension = url.split('.').pop()
+            const filename = path.join(filepath, `${id}.${extension}`)
+
+            if(fs.existsSync(filename)) {
+                console.log(`Skip ${filename}`)	
+                continue
+            }
+
+            try {
+                const body = await RequestAsync(options)
+                console.log(`Save ${filename}`)	
+                fs.writeFileSync(filename, body, 'binary')
+            }
+            catch (err) {
+                console.error(`Error When Fetching ${url}`)
+                errFiles.push({url: url, filename: filename})
+                fs.writeFileSync(errFilesPath, JSON.stringify(errFiles, null, 4))
+            }        
+        }    
+    }
+    catch (err) {
+        console.log(`Error On ${err}`)
+        console.log(`Abort.`)
+    }    
+}
+
+async function ProcessErr(data)
+{
+    const errData = []
+    for(let i = 0; i < data.length; ++i) {
+        const url = data[i].url
+        const filename = data[i].filename
         const options = { uri : url, encoding : 'binary' }
-        const extension = url.split('.').pop()
-        const filename = path.join(filepath, `${id}.${extension}`)
-
+        
         if(fs.existsSync(filename)) {
             console.log(`Skip ${filename}`)	
             continue
@@ -75,7 +109,30 @@ async function Process(url)
         }
         catch (err) {
             console.error(`Error When Fetching ${url}`)
-            console.log(JSON.stringify({url: url, filename: filename}, null, 4))
+            errData.push(data[i])
         }        
     }    
+
+    console.log(`Err = ${errData.length}`)
+    fs.writeFileSync(errFilesPath, JSON.stringify(errData, null, 4))
+}
+
+async function Process(url)
+{
+    if (url === 'err.json') {
+        // process urls in err.json
+        let data = []
+        try {
+            data = JSON.parse(fs.readFileSync(errFilesPath))
+        }
+        catch (err) {
+            console.log(`Error when parsing ${errFilesPath}, raw=<${err}>`)
+            console.log('Abort.')
+            return
+        }
+        return ProcessErr(data)
+    }
+    else {
+        return ProcessUrl(url)
+    }
 }
